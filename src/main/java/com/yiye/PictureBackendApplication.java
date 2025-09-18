7da9c1fe200f0614473dcbf285c2c99a3d640d56
龙一野 <2226548812@qq.com>
2025-09-18 23:21:02 +0800

    后端项目初始化，自定义通用基础代码

---
+++ src/main/java/com/yiye/PictureBackendApplication.java
@@ -0,0 +1,17 @@
+package com.yiye;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@MapperScan("com.yiye.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class PictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yiye/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.yiye.controller;
+
+import com.yiye.common.BaseResponse;
+import com.yiye.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    /**
+     * 测试接口
+     */
+    @GetMapping("/test")
+    public BaseResponse<String> test() {
+        return ResultUtils.success("test success");
+    }
+}
