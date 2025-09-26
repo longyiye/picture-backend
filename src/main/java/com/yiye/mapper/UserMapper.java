ab667a60e3d235b5bbc59efd4937d70549111921
龙一野 <2226548812@qq.com>
2025-09-26 16:19:06 +0800

    实现用户注册，用户登录，用户注销，用户管理功能

---
+++ src/main/java/com/yiye/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.yiye.mapper;
+
+import com.yiye.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author longyiye
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-09-26 15:16:04
+* @Entity com.yiye.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
