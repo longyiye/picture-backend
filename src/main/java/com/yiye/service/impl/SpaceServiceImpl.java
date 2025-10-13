cb57d7ac353639a0b4232cefe3094a1ea69e82b9
龙一野 <2226548812@qq.com>
2025-10-13 16:42:33 +0800

    实现空间资源使用分析，空间图片分类分析，空间图片标签分析，空间图片大小分析，用户上传行为分析，空间使用排行分析功能

--- src/main/java/com/yiye/service/impl/SpaceServiceImpl.java
+++ src/main/java/com/yiye/service/impl/SpaceServiceImpl.java
@@ -203,4 +203,12 @@ public class SpaceServiceImpl extends ServiceImpl<SpaceMapper, Space>
         }
     }
 
+    @Override
+    public void checkSpaceAuth(User loginUser, Space space) {
+        // 仅本人或管理员可编辑
+        if (!space.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+    }
+
 }
